8cd5b082166c0e1b7e8a0fd5a36c2c9830287741
alessandrodimich <alessandrodimich@hotmail.com>
2013-11-18 21:50:29 +0100

    improved css and corrected some bugs on password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -17,7 +17,7 @@ class PasswordResetsController < ApplicationController
 
   def edit
     begin
-      @user = User.find_by! password_reset_token: params[:id]
+      @user = User.find_by! password_reset_token: user_params
     rescue
       flash[:warning] = "Sorry, something went wrong!"
       redirect_to root_url
@@ -26,7 +26,7 @@ class PasswordResetsController < ApplicationController
 
   def update
 
-    @user = User.find_by! password_reset_token: params[:id]
+    @user = User.find_by! password_reset_token: user_params
     if @user
       @user.password = params[:user][:password]
       @user.password_confirmation = params[:user][:password_confirmation]
@@ -34,7 +34,7 @@ class PasswordResetsController < ApplicationController
     if @user.password_reset_sent_at < 2.hours.ago
       flash[:warning] = "Password reset has expired, please re-submit your email"
       redirect_to reset_password_path
-    elsif @user.save
+    elsif @user.update_attributes(password: @user.password, password_confirmation: @user.password_confirmation)
       login_user(@user, "remember_me_yes")
       flash[:success] = "Your password has been reset!"
       redirect_to root_url
@@ -50,6 +50,10 @@ private
       flash[:info] = "You are already logged in, Please change your password from the settings page"
       redirect_to root_url #or to the profile page
     end
+  end
+
+  def user_params
+     params.require(:id)
   end
 
 end
